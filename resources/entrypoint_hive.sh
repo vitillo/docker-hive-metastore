432aaac0966ade341f7f19132ba2a80c1ba00f43
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2016-08-08 18:46:09 +0100

    Increase postgresql connection timeout

--- resources/entrypoint_hive.sh
+++ resources/entrypoint_hive.sh
@@ -4,7 +4,7 @@ sh -c ./entrypoint.sh
 
 dockerize -template $HIVE_CONF/hive-site.xml.template:$HIVE_CONF/hive-site.xml \
           -wait tcp://$DB_URI:5432 \
-          -timeout 30s
+          -timeout 300s
 
 schematool -dbType postgres -initSchema
 
