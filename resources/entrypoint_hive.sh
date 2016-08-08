ab4f01d273bc1b4ee977258a14637754363a9ab9
Roberto Agostino Vitillo <ra.vitillo@gmail.com>
2016-08-08 18:39:01 +0100

    Use schematool to initialize the metastore

--- resources/entrypoint_hive.sh
+++ resources/entrypoint_hive.sh
@@ -6,8 +6,6 @@ dockerize -template $HIVE_CONF/hive-site.xml.template:$HIVE_CONF/hive-site.xml \
           -wait tcp://$DB_URI:5432 \
           -timeout 30s
 
-pushd $HIVE_HOME/scripts/metastore/upgrade/postgres/
-PGPASSWORD=$DB_PASSWORD psql -h $DB_URI -U $DB_USER -d metastore -f hive-schema-${HIVE_VERSION}.postgres.sql
-popd
+schematool -dbType postgres -initSchema
 
 exec $@
